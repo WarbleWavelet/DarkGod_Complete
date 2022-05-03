3435fb7ea293d57c15d6896f8141a6c3679bef44
吴伟升 <3027565221@qq.com>
2022-05-03 23:58:47 +0800

    数据库的查

--- SqlTest_2017/SqlTest_2017/Program.cs
+++ SqlTest_2017/SqlTest_2017/Program.cs
@@ -19,9 +19,9 @@ class Program
                 Console.WriteLine("连不上" + ex);
             }
         }
-        Add();
+        //Add();
         //Delete();
-        //Query();
+        Query();
         //Update();
 
         Console.ReadKey();
@@ -32,21 +32,39 @@ class Program
 
     static void Add()
     {
-        MySqlCommand cmd = new MySqlCommand("insert into userinfo set name='黄忠',age=40", conn);
+        MySqlCommand cmd = new MySqlCommand("insert into userinfo set name='张辽',age=40", conn);
         cmd.ExecuteNonQuery();
-
-        Console.WriteLine("已增");
+        int id = (int)cmd.LastInsertedId;
+        Console.WriteLine("已增id：{0}",id);
     }
+
+
     static void Delete()
     {
         MySqlCommand cmd = new MySqlCommand("", conn);
     }
+
+
     static void Query()
     {
-        MySqlCommand cmd = new MySqlCommand("", conn);
+        //MySqlCommand cmd = new MySqlCommand("select * from userinfo", conn);
+        MySqlCommand cmd = new MySqlCommand("select * from userinfo where name='赵云'", conn);
+        MySqlDataReader reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            int id = reader.GetInt32("id");
+            string name = reader.GetString("name");
+            int age = reader.GetInt32("age");
+            Console.WriteLine(String.Format("已查：id:{0} name:{1} age:{2}", id, name, age));
+
+        }
     }
+
+
     static void Update()
     {
-        MySqlCommand cmd = new MySqlCommand("", conn);
+
     }
+
+
 }
