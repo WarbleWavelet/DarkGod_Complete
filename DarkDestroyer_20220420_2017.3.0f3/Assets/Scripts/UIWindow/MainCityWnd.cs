0e09a443e9b620d8feca979b60ee862ef39eb31b
吴伟升 <3027565221@qq.com>
2022-05-05 16:29:13 +0800

    主城的RefreshUI

--- DarkDestroyer_20220420_2017.3.0f3/Assets/Scripts/UIWindow/MainCityWnd.cs
+++ DarkDestroyer_20220420_2017.3.0f3/Assets/Scripts/UIWindow/MainCityWnd.cs
@@ -6,14 +6,36 @@
 	功能：主城市UI界面
 *****************************************************/
 
+using PEProtocol;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainCityWnd : WindowRoot
 {
- 
+    public Image imgPowerPrg;
+   // public Image imgExpPrg;
+    public Text txtFight;
+    public Text txtExpPrg;
+    public Text txtPower;
+    public Text txtLevel;
+    public Text txtName;
+
     protected override void InitWnd()
     {
         base.InitWnd();
+
+        RefreshUI();
        
+    }
+
+    private void RefreshUI()
+    {
+        PlayerData pd = GameRoot.Instance.PlayerData;
+        SetText(txtFight, PECommon.GetFightByProps(pd));
+        SetText(txtPower,"体力:"+pd.power+"/"+PECommon.GetPowerLimit(pd.lv));
+        imgPowerPrg.fillAmount = (pd.power * 1.0f) / PECommon.GetPowerLimit(pd.lv);
+        SetText(txtName, pd.name);
+        SetText(txtLevel, pd.lv);
     }
 }
--- Server/PEProtocol/PECommoncs.cs
+++ Server/PEProtocol/PECommoncs.cs
@@ -1,4 +1,5 @@
 ﻿using PENet;
+using PEProtocol;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,28 @@ public class PECommon
     {
         LogLevel lv = (LogLevel)tp;
         PETool.LogMsg(msg,lv);
+    }
+
+    /// <summary>
+    /// 战力 
+    /// </summary>
+    /// <param name="pd"></param>
+    /// <returns></returns>
+    public static int GetFightByProps( PlayerData pd)
+    {
+        return pd.lv * 100 + pd.ap + pd.ad + pd.addef + pd.apdef;
+
+    }
+
+    /// <summary>
+    /// 每10级体力上限+150
+    /// </summary>
+    /// <param name="lv"></param>
+    /// <returns></returns>
+
+    public static int GetPowerLimit(int lv)
+    {
+        return ((lv - 1) / 10) * 150 + 150;
     }
 }
 
